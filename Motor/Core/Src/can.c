2bbee6bc37ca3a206e712ae72f4ab2d2c71efaad
CandyLOVO <1565630950@qq.com>
2023-09-27 20:37:10 +0800

    There are still errorsgit add .git add .git add .

---
+++ Motor/Core/Src/can.c
@@ -0,0 +1,169 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    can.c
+  * @brief   This file provides code for the configuration
+  *          of the CAN instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "can.h"
+
+/* USER CODE BEGIN 0 */
+static CAN_TxHeaderTypeDef can_tx_message;
+static CAN_TxHeaderTypeDef can_rx_message;
+static uint8_t can_send_data[8];
+/* USER CODE END 0 */
+
+CAN_HandleTypeDef hcan1;
+
+/* CAN1 init function */
+void MX_CAN1_Init(void)
+{
+
+  /* USER CODE BEGIN CAN1_Init 0 */
+
+  /* USER CODE END CAN1_Init 0 */
+
+  /* USER CODE BEGIN CAN1_Init 1 */
+
+  /* USER CODE END CAN1_Init 1 */
+  hcan1.Instance = CAN1;
+  hcan1.Init.Prescaler = 3;
+  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_10TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_3TQ;
+  hcan1.Init.TimeTriggeredMode = DISABLE;
+  hcan1.Init.AutoBusOff = DISABLE;
+  hcan1.Init.AutoWakeUp = DISABLE;
+  hcan1.Init.AutoRetransmission = DISABLE;
+  hcan1.Init.ReceiveFifoLocked = DISABLE;
+  hcan1.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN1_Init 2 */
+//	CAN_FilterTypeDef can_filter;
+//	can_filter.FilterBank = 0;
+//	can_filter.FilterMode = CAN_FILTERMODE_IDMASK;
+//	can_filter.FilterScale = CAN_FILTERSCALE_32BIT;
+//	can_filter.FilterIdHigh = 0;
+//	can_filter.FilterIdLow = 0;
+//	can_filter.FilterMaskIdHigh = 0;
+//	can_filter.FilterMaskIdLow = 0;
+//	can_filter.FilterFIFOAssignment = CAN_RX_FIFO0;
+//	can_filter.SlaveStartFilterBank = 14;
+//	HAL_CAN_ConfigFilter(&hcan1,&can_filter);
+//	HAL_CAN_ActivateNotification(&hcan1,CAN_IT_RX_FIFO0_MSG_PENDING);
+//	HAL_CAN_Start(&hcan1);
+	
+    CAN_FilterTypeDef  can_filter;
+
+    can_filter.FilterBank = 0;                       // filter 0
+    can_filter.FilterMode =  CAN_FILTERMODE_IDMASK;  // 标识符屏蔽位模式
+    can_filter.FilterScale = CAN_FILTERSCALE_32BIT;		//过滤器位宽为单个32使
+    can_filter.FilterIdHigh = 0;//标识符寄存器 
+    can_filter.FilterIdLow  = 0;//标识符寄存器 
+    can_filter.FilterMaskIdHigh = 0;//屏蔽寄存噿
+    can_filter.FilterMaskIdLow  = 0;       //屏蔽寄存噿   set mask 0 to receive all can id
+    can_filter.FilterFIFOAssignment = CAN_RX_FIFO0; // assign to fifo0，接收器是FIFO0
+//    can_filter.FilterActivation = ENABLE;           // enable can filter
+    can_filter.SlaveStartFilterBank  = 14;          
+   
+    HAL_CAN_ConfigFilter(&hcan1, &can_filter);        // init can filter
+    HAL_CAN_ActivateNotification(&hcan1,CAN_IT_RX_FIFO0_MSG_PENDING);		//使能can的FIFO0中断
+    HAL_CAN_Start(&hcan1);//启动can1
+  /* USER CODE END CAN1_Init 2 */
+
+}
+
+void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(canHandle->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspInit 0 */
+
+  /* USER CODE END CAN1_MspInit 0 */
+    /* CAN1 clock enable */
+    __HAL_RCC_CAN1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    /**CAN1 GPIO Configuration
+    PD0     ------> CAN1_RX
+    PD1     ------> CAN1_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF9_CAN1;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    /* CAN1 interrupt Init */
+    HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
+  /* USER CODE BEGIN CAN1_MspInit 1 */
+
+  /* USER CODE END CAN1_MspInit 1 */
+  }
+}
+
+void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
+{
+
+  if(canHandle->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspDeInit 0 */
+
+  /* USER CODE END CAN1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_CAN1_CLK_DISABLE();
+
+    /**CAN1 GPIO Configuration
+    PD0     ------> CAN1_RX
+    PD1     ------> CAN1_TX
+    */
+    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_0|GPIO_PIN_1);
+
+    /* CAN1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
+  /* USER CODE BEGIN CAN1_MspDeInit 1 */
+
+  /* USER CODE END CAN1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+void can_cmd_send(int motor1,int motor2,int motor3,int motor4)
+{
+	uint32_t send_mail_box;
+	can_tx_message.StdId = 0x200;
+	can_tx_message.IDE = CAN_ID_STD;
+	can_tx_message.RTR = CAN_RTR_DATA;
+	can_tx_message.DLC = 0x08;
+	can_send_data[0] = (motor1>>8)&0xff;
+	can_send_data[1] = motor1&0xff;
+	can_send_data[2] = (motor2>>8)&0xff;
+	can_send_data[3] = motor2&0xff;
+	can_send_data[4] = (motor3>>8)&0xff;
+	can_send_data[5] = motor3&0xff;
+	can_send_data[6] = (motor4>>8)&0xff;
+	can_send_data[7] = motor4&0xff;
+	HAL_CAN_AddTxMessage(&hcan1,&can_tx_message,can_send_data,&send_mail_box);
+}
+/* USER CODE END 1 */
